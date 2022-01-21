78a00acdd51228c0ec546206f153abd1328f8881
Etka Taskin <79757579+taskintx@users.noreply.github.com>
2022-01-21 10:31:27 -0600

    branch16

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Dashboard />
+        <Router>
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard}></Route>
+          <Route exact path="/addProject" component={AddProject}></Route>
+        </Router>
       </div>
     );
   }
