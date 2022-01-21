4874233a8514ad985ec9e721436a02df6dd85e4e
Etka Taskin <79757579+taskintx@users.noreply.github.com>
2022-01-20 23:55:47 -0600

    branch15

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
-import Dashboard from './components/Dashboard';
-import Header from './components/Layout/Header';
+import React, { Component } from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-
-      <Header />
-      <Dashboard />
-      
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Dashboard />
+      </div>
+    );
+  }
 }
 
 export default App;
