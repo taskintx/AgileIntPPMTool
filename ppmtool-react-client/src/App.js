edc3d79ab32eee7a715484df0737917743f62105
Etka Taskin <79757579+taskintx@users.noreply.github.com>
2022-01-20 23:08:02 -0600

    branch13

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
 
 function App() {
   return (
     <div className="App">
+
+      <Header />
       <Dashboard />
-      <Dashboard />
-      <Dashboard />
+      
 
     </div>
   );
