778c8e9057a9499e2fb5f2b8a9d3fb5d07bbef58
Etka Taskin <79757579+taskintx@users.noreply.github.com>
2022-01-23 18:58:07 -0600

    branch19

--- PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package io.agileintelligence.ppmtool.exceptions;
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,11 +12,11 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 @RestController
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
-	
-	@ExceptionHandler
-	public final ResponseEntity<Object> handleProjectIdException(ProjectIdException ex, WebRequest request){
-		ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(ex.getMessage());
-		return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
-	}
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleProjectIdException(ProjectIdException ex, WebRequest request){
+        ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(ex.getMessage());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
 
 }
