2d43e461c01434a535e77809d9776eaadb6c3b5f
Etka Taskin <79757579+taskintx@users.noreply.github.com>
2022-01-24 21:18:43 -0600

    branch25

--- ppmtool-react-client/src/components/Project/UpdateProject.js
+++ ppmtool-react-client/src/components/Project/UpdateProject.js
@@ -1,138 +1,69 @@
 import React, { Component } from "react";
+import { getProject } from "../../actions/projectActions";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { createProject } from "../../actions/projectActions";
 import classnames from "classnames";
 
-class AddProject extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      projectName: "",
-      projectIdentifier: "",
-      description: "",
-      start_date: "",
-      end_date: "",
-      errors: {},
-    };
-
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
+class UpdateProject extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getProject(id, this.props.history);
   }
-
-  //life cycle hooks
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.errors) {
-      this.setState({ errors: nextProps.errors });
-    }
-  }
-
-  onChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
-    const newProject = {
-      projectName: this.state.projectName,
-      projectIdentifier: this.state.projectIdentifier,
-      description: this.state.description,
-      start_date: this.state.start_date,
-      end_date: this.state.end_date,
-    };
-    this.props.createProject(newProject, this.props.history);
-  }
-
   render() {
-    const { errors } = this.state;
-
     return (
-      <div>
-        <div className="project">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-8 m-auto">
-                <h5 className="display-4 text-center">Update Project form</h5>
-                <hr />
-                <form onSubmit={this.onSubmit}>
-                  <div className="form-group">
-                    <input
-                      type="text"
-                      className={classnames("form-control form-control-lg", {
-                        "is-invalid": errors.projectName,
-                      })}
-                      placeholder="Project Name"
-                      name="projectName"
-                      value={this.state.projectName}
-                      onChange={this.onChange}
-                    />
-                    {errors.projectName && (
-                      <div className="invalid-feedback">
-                        {errors.projectName}
-                      </div>
-                    )}
-                  </div>
-                  <div className="form-group">
-                    <input
-                      type="text"
-                      className={classnames("form-control form-control-lg", {
-                        "is-invalid": errors.projectIdentifier,
-                      })}
-                      placeholder="Unique Project ID"
-                      name="projectIdentifier"
-                      value={this.state.projectIdentifier}
-                      onChange={this.onChange}
-                    />
-                    {errors.projectIdentifier && (
-                      <div className="invalid-feedback">
-                        {errors.projectIdentifier}
-                      </div>
-                    )}
-                  </div>
-                  <div className="form-group">
-                    <textarea
-                      className={classnames("form-control form-control-lg", {
-                        "is-invalid": errors.description,
-                      })}
-                      placeholder="Project Description"
-                      name="description"
-                      value={this.state.description}
-                      onChange={this.onChange}
-                    />
-                    {errors.description && (
-                      <div className="invalid-feedback">
-                        {errors.description}
-                      </div>
-                    )}
-                  </div>
-                  <h6>Start Date</h6>
-                  <div className="form-group">
-                    <input
-                      type="date"
-                      className="form-control form-control-lg"
-                      name="start_date"
-                      value={this.state.start_date}
-                      onChange={this.onChange}
-                    />
-                  </div>
-                  <h6>Estimated End Date</h6>
-                  <div className="form-group">
-                    <input
-                      type="date"
-                      className="form-control form-control-lg"
-                      name="end_date"
-                      value={this.state.end_date}
-                      onChange={this.onChange}
-                    />
-                  </div>
-
+      <div className="project">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h5 className="display-4 text-center">Update Project form</h5>
+              <hr />
+              <form>
+                <div className="form-group">
                   <input
-                    type="submit"
-                    className="btn btn-primary btn-block mt-4"
+                    type="text"
+                    className="form-control form-control-lg "
+                    placeholder="Project Name"
+                    name="projectName"
+                    value={this.state.projectName}
                   />
-                </form>
-              </div>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Unique Project ID"
+                    name="projectIdentifier"
+                    disabled
+                  />
+                </div>
+                <div className="form-group">
+                  <textarea
+                    className="form-control form-control-lg"
+                    placeholder="Project Description"
+                    name="description"
+                  />
+                </div>
+                <h6>Start Date</h6>
+                <div className="form-group">
+                  <input
+                    type="date"
+                    className="form-control form-control-lg"
+                    name="start_date"
+                  />
+                </div>
+                <h6>Estimated End Date</h6>
+                <div className="form-group">
+                  <input
+                    type="date"
+                    className="form-control form-control-lg"
+                    name="end_date"
+                  />
+                </div>
+                <input
+                  type="submit"
+                  className="btn btn-primary btn-block mt-4"
+                />
+              </form>
             </div>
           </div>
         </div>
@@ -141,13 +72,13 @@ class AddProject extends Component {
   }
 }
 
-AddProject.propTypes = {
-  createProject: PropTypes.func.isRequired,
-  errors: PropTypes.object.isRequired,
+UpdateProject.propTypes = {
+  getProject: PropTypes.func.isRequired,
+  project: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-  errors: state.errors,
+  project: state.project.project,
 });
 
-export default connect(mapStateToProps, { createProject })(AddProject);
+export default connect(mapStateToProps, { getProject })(UpdateProject);
--- ppmtool-react-client/src/reducers/projectReducer.js
+++ ppmtool-react-client/src/reducers/projectReducer.js
@@ -1,16 +1,21 @@
-import { GET_PROJECTS } from "../actions/types";
+import { GET_PROJECTS, GET_PROJECT } from "../actions/types";
 
 const initialState = {
   projects: [],
   project: {},
 };
-
 export default function (state = initialState, action) {
   switch (action.type) {
     case GET_PROJECTS:
       return {
         ...state,
         projects: action.payload,
+      };
+
+    case GET_PROJECT:
+      return {
+        ...state,
+        project: action.payload,
       };
     default:
       return state;
