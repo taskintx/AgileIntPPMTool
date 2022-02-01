ecacd1a9d3ad76acaab71c48461c1aeaed6ee840
Etka Taskin <79757579+taskintx@users.noreply.github.com>
2022-02-01 12:43:03 -0600

    branch57

--- ppmtool-react-client/src/actions/backlogActions.js
+++ ppmtool-react-client/src/actions/backlogActions.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { GET_ERRORS, GET_BACKLOG, GET_PROJECT_TASK } from "./types";
+import {
+  GET_ERRORS,
+  GET_BACKLOG,
+  GET_PROJECT_TASK,
+  DELETE_PROJECT_TASK,
+} from "./types";
 
 //Fix bug with priority in Spring Boot Server, needs to check null first
 export const addProjectTask =
@@ -63,3 +68,17 @@ export const updateProjectTask =
       });
     }
   };
+
+export const deleteProjectTask = (backlog_id, pt_id) => async (dispatch) => {
+  if (
+    window.confirm(
+      `You are deleting project task ${pt_id}, this action cannot be undone`
+    )
+  ) {
+    await axios.delete(`/api/backlog/${backlog_id}/${pt_id}`);
+    dispatch({
+      type: DELETE_PROJECT_TASK,
+      payload: pt_id,
+    });
+  }
+};
